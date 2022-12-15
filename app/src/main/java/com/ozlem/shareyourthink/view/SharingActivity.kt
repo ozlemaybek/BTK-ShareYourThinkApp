726f7a5248ee9964e648137dd6dc3c6e05641609
Özlem Katibe AYBEK <ozlemkatibeaybek@gmail.com>
2022-12-15 22:11:00 +0300

    Project updated.

--- app/src/main/java/com/ozlem/shareyourthink/view/SharingActivity.kt
+++ app/src/main/java/com/ozlem/shareyourthink/view/SharingActivity.kt
@@ -1,9 +1,19 @@
 package com.ozlem.shareyourthink.view
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.firebase.Timestamp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
@@ -19,6 +29,11 @@ class SharingActivity : AppCompatActivity() {
     // Authentication objesi oluşturalım:
     // auth isimli objenin bir FirebaseAuth objesi olacağını söyledik:
     private lateinit var auth : FirebaseAuth
+    // onActivityResult metodu için:
+    // choosenImage ilk başta bir Uri nullable olacak ve ilk değeri null olacak.
+    var choosenImage : Uri? = null
+    // bitmap android'de görselleri tuttuğumuz sınıftır.
+    var choosenBitmap : Bitmap? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -55,5 +70,95 @@ class SharingActivity : AppCompatActivity() {
         }.addOnFailureListener{ exception ->
             Toast.makeText(this, exception.localizedMessage, Toast.LENGTH_LONG).show()
         }
+    }
+
+    fun addImageButtonOnClick(view : View){
+        // Butona tıklandığında önce kullanıcıdan izin istemeliyiz:
+        // if içindeki Manifest'i android olan seçmeye dikkat etmeliyiz.
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            // Bu if'in içine girildiyse izin verilmemiş ve izin istememiz gerekiyor demektir.
+            // requestPermissions() : izinleri iste demektir.
+            // Hangi izinleri isteyeceğimizi bir dizi olarak vermemiz gerekiyor.
+            // Tek izin isteyeceğiz ama yinede dizi olarak vermemiz gerekiyor.
+            // Son parametrede bizeden integer bir istek kodu istiyor. requestCode yapılan izin isteğinin kodu.
+            // Bu kodu kullanarak sonrasında bu izinden mi bahsediyoruz kontrol edebileceğiz.
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 1)
+        }else{
+            // İzin zaten verilmiş direkt galeriye gidebiliriz:
+            // Galeriye gidelim:
+            // PICK : almak demektir.
+            // 3.parametre: veriyi nereden alacağım.
+            // galleryIntent içinde galeride kayıtlı resmin konumunu tutacak:
+            val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            // Daha önce startActivity kullanıyorduk fakat şimdi startActivityForResult() kullandık.
+            // ForResult sonucunda bir cevap dönecek ve ben bu cevabı işleyeceğim demek:
+            startActivityForResult(galleryIntent,2)
+
+        }
+    }
+
+    // İzin verilirse ne yapacağız:
+    // Aşağıdaki fonksiyon izinlerin sonucunda çağrılacak fonksiyondur.
+    // Bu fonksiyonun içinde bize bir requestCode, izinler ve grantResults yani bana geri dönülen cevaplar veriliyor.
+    // grantResults: bana gelen cevaplar. Eğer bana bir cevap gelmediyse veya izin verilmediyse bir şey yapmama gerek yok.
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        // Aynı activity ya da fragment içinde birden fazla izin isteği olabilir bu yüzden requestCode'u kontrol edelim:
+        if(requestCode == 1){
+            if(grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                // Bu kod bloğunun içine girildiyse izin verildiği kesinleşmiştir.
+                // Aynı şey ile ilgili requestCode'un aynı olması önemli yukarıda requestCode'a 2 verdiğimiz için burada da 2 verdik.
+                val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                startActivityForResult(galleryIntent,2)
+            }
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    // Daha önce galeri izni verildiyse ve direkt galeriye gidilip resim seçildiyse:
+    // Aşağıdaki fonksiyon yapılan işlemin sonucunda bize ne döndürüldüğünü gösteriyor:
+    // Bu fonksiyon bizden 3 parametre istiyor.
+    // 1.parametre: requestCode (istek kodu)
+    // 2.parametre: resultCode (sonuç kodu)
+    // 3.parametre: data (geri döndürülen data)
+    // RESULT_OK : result okey: kullanıcı düzgün bir cevap geri döndü demektir.
+    // RESULT_CANCELLED : kullanıcı herhangi bir görsel seçmekten vazgeçti.
+    // data != null : geri dönen data null'a eşit değil.
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if(requestCode == 2 && resultCode == RESULT_OK && data != null){
+            // Eğer iif içindeki 3 şartta sağlanıyorsa kullanıcı bize düzgün bir cevap geri dönmüştür.
+            // Şimdi geri dönen veriyi alalım:
+            // data.data dediğimizde bu bize bir Uri verir.
+            // Uri: seçilen görselin telefon içerisinde kaydedildiği konum. Bu konum ile işlemlerimizi yapabiliyoruz.
+            // choosenImage içinde Uri'ı tutuyoruz:
+            choosenImage = data.data
+            // imageView'u xml tarafında görünmez yapmıştık şimdi görünür yapmalıyız:
+            imageViewID.visibility = View.VISIBLE
+            if(choosenImage != null){
+                // Öncelikle Uri'ı bitmap'e çevirmemiz gerekecek fakat bu konuda API 28 ve sonrası için farklılıklar var.
+                // Bu yüzden 28 ve sonrası için yapacaklarımızı if bloğunda 28 öncesi için yapacaklarımızı ise else bloğunda yazıyoruz:
+                // Build.VERSION.SDK_INT = Android işletim sisteminin versiyonu
+                if(Build.VERSION.SDK_INT >= 28){
+                    // Önce bir source yani kaynak oluşturmamız gerekiyor.
+                    // Bu işlemi android.graphics içindeki ImageDecoder sınıfını kullanarak yapmamız gerekiyor.
+                    // createSource() diyerek bir source oluşturuyoruz.Bizden parametre olarak bir contentResolver birde Uri istiyor.
+                    val source = ImageDecoder.createSource(this.contentResolver, choosenImage!!)
+                    choosenBitmap = ImageDecoder.decodeBitmap(source)
+                    imageViewID.setImageBitmap(choosenBitmap)
+                } else{
+                    // getBitmap() deprecated yani tedahülden kalkmış bir metod fakat API 28 öncesinde çalırken 28 ve sonrasında çalışmıyor.
+                    // getBitmap bizden bir contentResolver yani içerik çözümleyici istiyor. Bunu this.contentResolver diyerek alabiliriz.
+                    // 2. parametre olarakta çözümlenecek bir Uri veriyoruz:
+                    choosenBitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, choosenImage)
+                    // Seçilen bitmap'i imageView içinde gösterelim:
+                    imageViewID.setImageBitmap(choosenBitmap)
+                }
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
